83a3b17cb56e57783f187c3cb77794f795b52230
Liam Fritzson <liam.fritzson@fresenius-kabi.com>
2025-01-15 15:55:24 +0100

    Change

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,20 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations.Schema;
 using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
     public class ApplicationDBContext : DbContext
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
-        : base(dbContextOptions)
-        {
-
-        }
+            : base(dbContextOptions) { }
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
